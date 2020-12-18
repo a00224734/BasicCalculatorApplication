8d2d99e71aa57f744708393c7d13c1e4543ff1be
Gagandeep Kaur <a00224734@mycambrian.ca>
2020-12-18 23:52:43 +0530

    Initial commit

---
+++ BasicCalApp_A00224734_GagandeepKaur/Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BasicCalApp_A00224734_GagandeepKaur.Models;
+
+namespace BasicCalApp_A00224734_GagandeepKaur.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: CalculatorController
+        public ActionResult Index()
+        {
+            return View(new Calculator());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calculator calculator, string operate)
+        {
+            calculator.result = 0;
+            switch (operate)
+            {
+                case "add":
+                    calculator.result = calculator.value1 + calculator.value2;
+                    break;
+                case "subtract":
+                    calculator.result = calculator.value1 - calculator.value2;
+                    break;
+                case "multiply":
+                    calculator.result = calculator.value1 * calculator.value2;
+                    break;
+                case "divide":
+                    if (calculator.value2 == 0)
+                    {
+                        calculator.error = "You divided a value by zero. Enter another value.";
+                    }
+                    else
+                    {
+                        calculator.result = calculator.value1 / calculator.value2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return View(calculator);
+        }
+    }
+}
---
+++ BasicCalApp_A00224734_GagandeepKaur/Models/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicCalApp_A00224734_GagandeepKaur.Models
+{
+    public class Calculator
+    {
+        public double value1 { get; set; }
+        public double value2 { get; set; }
+        public double result { get; set; }
+        public String error { get; set; }
+    }
+}
